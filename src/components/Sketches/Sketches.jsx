fa6f8f218ef6e77236e54ab671b67c5ccb12f7b6
preranagiri28@gmail.com <122023933+rijanmajhi21@users.noreply.github.com>
2024-11-21 11:43:56 +0545

    Commit

--- src/components/Sketches/Sketches.jsx
+++ src/components/Sketches/Sketches.jsx
@@ -7,27 +7,27 @@ function Sketches() {
         <h1 className="section-title">Sketching Gallery</h1>
         <div className="sketches-gallery">
         <div className="sketch-card">
-            <img src="/images/image5.jpg" alt="Artwork 1" className="sketch-image" />
+            <img src="/images/Image5.jpg" alt="Artwork 1" className="sketch-image" />
             <div className="art-name">Heart</div>
         </div>
         <div className="sketch-card">
-            <img src="/images/image8.jpg" alt="Artwork 2" className="sketch-image" />
+            <img src="/images/Image8.jpg" alt="Artwork 2" className="sketch-image" />
             <div className="art-name">Ageing</div>
         </div>
         <div className="sketch-card">
-            <img src="/images/image9.jpg" alt="Artwork 3" className="sketch-image" />
+            <img src="/images/Image9.jpg" alt="Artwork 3" className="sketch-image" />
             <div className="art-name">Lips work</div>
         </div>
         <div className="sketch-card">
-            <img src="/images/image11.jpg" alt="Artwork 1" className="sketch-image" />
+            <img src="/images/Image11.jpg" alt="Artwork 4" className="sketch-image" />
             <div className="art-name">Photo Vs. Drawing</div>
         </div>
         <div className="sketch-card">
-            <img src="/images/image12.jpg" alt="Artwork 2" className="sketch-image" />
+            <img src="/images/Image12.jpg" alt="Artwork 5" className="sketch-image" />
             <div className="art-name">Eye</div>
         </div>
         <div className="sketch-card">
-            <img src="/images/Shiva.jpg" alt="Artwork 3" className="sketch-image" />
+            <img src="/images/Shiva.jpg" alt="Artwork 6" className="sketch-image" />
             <div className="art-name">Lord Shiva</div>
         </div>
         </div>
